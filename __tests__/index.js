3d2f4bff8eda59630604896679a28fb2b5553d97
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-06-01 20:35:40 +0200

    test(index): add for render app

---
+++ __tests__/index.js
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import App from '@hexlet/react-todo-app-with-backend';
+import React from 'react';
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('todo test', () => {
+  it('should render application', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('Hexlet Todos')).toBeInTheDocument();
+  });
+});
