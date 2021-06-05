b63d5b4baf2f73f1935e0cf28d580534ebf7be3b
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-06-05 13:12:06 +0200

    test(tests): check create task

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import App from '@hexlet/react-todo-app-with-backend';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
+
+const initialState = {
+  currentListId: 1,
+  lists: [{ id: 0, name: 'primary', removable: false }],
+  tasks: [],
+};
+
+const server = setupServer(
+  rest.post('/api/v1/lists/:id/tasks', (req, res, ctx) => {
+    const task = {
+      completed: false,
+      id: 3,
+      listId: Number(req.params.id),
+      text: req.body.text,
+      touched: 1622887145394,
+    };
+
+    return res(ctx.json(task));
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('todo test', () => {
   it('should render application', () => {
@@ -9,5 +36,14 @@ describe('todo test', () => {
     expect(getByText('Hexlet Todos')).toBeInTheDocument();
     expect(getByRole('textbox', {  name: /new list/i})).toBeInTheDocument();
     expect(getByRole('textbox', {  name: /new task/i})).toBeInTheDocument();
+  });
+
+  it('should create task', async () => {
+    const { getByRole, getByText } = render(<App {...initialState} />);
+
+    userEvent.type(getByRole('textbox', {  name: /new task/i}), 'test');
+    userEvent.click(getByRole('button', {  name: /add/i}));
+
+    expect(await waitFor(() => getByText('test'))).toBeInTheDocument();
   });  
 });
