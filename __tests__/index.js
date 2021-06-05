c05fde25057205c61728d70fc6ad5c17d31c0497
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-06-05 13:01:43 +0200

    test(tests): check render app

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,15 +1,6 @@
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react';
-import { setupServer } from 'msw/node';
 import App from '@hexlet/react-todo-app-with-backend';
 import React from 'react';
-
-const server = setupServer();
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
 describe('todo test', () => {
   it('should render application', () => {
@@ -18,5 +9,5 @@ describe('todo test', () => {
     expect(getByText('Hexlet Todos')).toBeInTheDocument();
     expect(getByRole('textbox', {  name: /new list/i})).toBeInTheDocument();
     expect(getByRole('textbox', {  name: /new task/i})).toBeInTheDocument();
-  });
+  });  
 });
